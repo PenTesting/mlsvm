3a7ff71e888d2d8d478fc41aa9b09c514aa2f5ab
esfp <esadrfa@g.clemson.edu>
2018-08-30 21:07:52 -0400

    the test file name  is fixed

--- src/config_params.cc
+++ src/config_params.cc
@@ -581,8 +581,7 @@ void Config_params::set_fixed_file_names(){
     n_norm_data_f_name  = get_ds_path() + get_ds_name() + "_maj_norm_data.dat";
 
     test_ds_f_name      = get_ds_path() + get_test_name() + "_label_data_test.dat";
-    std::cout << "get_test_ds_f_name(): " << get_test_ds_f_name() << "\n\n";
-    exit (1);
+//    std::cout << "get_test_ds_f_name(): " << get_test_ds_f_name() << "\n\n";
 }
 
 
